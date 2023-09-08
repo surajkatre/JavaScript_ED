42d129df8f465480dc6e6d3cc8764ff7492d065a
surajkatre <surajkatre07@gmail.com>
2023-09-08 11:44:31 +0530

    new file add

--- 03_Datatype_conversion.js
+++ 03_Datatype_conversion.js
@@ -43,4 +43,40 @@ let somevar = 33
 
 let value = 33;
 let  negVal= -value
-console.log(negVal)
+// console.log(negVal)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " suraj"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// // console.log(+true+);
+// console.log(true);
+// console.log(+" " );
+
+let num1, num2, num3 
+num1 = num2 = num3 =2+2
+
+// console.log(num1);
+
+
+let gCounter = 100
+++gCounter
+// gCounter++
+console.log(gCounter);
+// prefix and postfix in js mdn 
