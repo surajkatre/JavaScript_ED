5209eadae4b33d339915c7b475ea06e8ae770916
surajkatre <surajkatre07@gmail.com>
2023-09-08 11:46:39 +0530

    file add

---
+++ 04_datatype_Comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+
+
+// console.log("2" >1 );
+// console.log(null <0);
+// console.log(null ==0);
+// console.log(null >=0);
+// console.log(undefined < null);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+
+// console.log(1==2);
+// console.log(1===2);
+
+// ===
+// check value and datatype as well 
+
+console.log("2" ===2); //false
+console.log("2" ==2); //true
+
+
---
+++ Datatypes_Summary.js
@@ -0,0 +1,52 @@
+// data category they way data is stored and the way it is access
+// premitive and non premitive
+
+/*seven category :
+String , Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+2. Reference/ non premitive type
+Array, Objects,Functions
+
+
+*/
+
+// const score  = 21;
+// const scoreVal  = 21.2;
+// let email ; // undefined
+
+// const isLogged = false
+// const outSideTemo = null
+
+// console.log(typeof score);
+
+// const id = Symbol('123')
+
+// const anId = Symbol('123')
+
+// console.log(id ===  anId);
+
+
+
+
+const heroes = ["Ram", "shyam", "honeyPaji"]
+
+console.log(heroes[1]);
+
+   let mypersonal ={
+    name:"Suraj",
+    age : 22
+}
+console.log(mypersonal);
+
+
+
+// function 
+
+const myfun = function() {
+    console.log("mera yeshu");
+}
+
+// console.log(myfun);
+console.log(typeof(myfun));
