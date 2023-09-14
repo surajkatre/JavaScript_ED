fbcfad40814ca5264d14e73c1874ea9578e49808
suraj.katre@pragmatyc.com <suraj.katre@pragmatyc.com>
2023-09-14 12:44:34 +0530

    file add

---
+++ 02_Basic/array.js
@@ -0,0 +1,49 @@
+const arr  = [0,1,2,3,4,5]
+// console.log(arr[2]);
+//shallow copies and deep copy
+
+
+const myheroes = ["beatle", "shaktiman",4,7,2,8]
+
+const marvel = new Array(1,2,3,4)
+// console.log(marvel.length );
+
+
+// array methods
+
+
+// myheroes.push("Suraj")
+// myheroes.push("Don")
+// myheroes.pop()
+
+  
+// myheroes.unshift(0)
+// myheroes.shift()
+// console.log(myheroes.includes(2)); 
+
+
+// console.log(myheroes.indexOf(2)); 
+
+
+const memo = arr.join()
+
+// console.log(typeof memo);
+// console.log( memo);
+
+// console.log(typeof arr);
+
+
+
+// slice m splice
+
+console.log("A " , arr);
+
+const n1 = arr.slice(1,3)
+console.log(n1);
+console.log("B ", arr);
+
+const n2 = arr.splice(1,3)
+console.log("C ", arr);
+console.log(n2);
+//splice will manipulate the original array and dlt the values
+ 
