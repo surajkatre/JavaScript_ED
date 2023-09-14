8d7e842c3aff02bf7031223b62e0be0c4c928f46
suraj.katre@pragmatyc.com <suraj.katre@pragmatyc.com>
2023-09-14 14:21:36 +0530

    file add

---
+++ 02_Basic/02_array.js
@@ -0,0 +1,13 @@
+const marvel_heros =[ "thor", "Hulk","IronMan"]
+const DC_Heors = ["Superman","WonderWOmen", "flash"]
+
+// marvel_heros.push(DC_Heors)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// console.log(marvel_heros[3]);
+// console.log(marvel_heros[3]);
+
+
+ const MC =marvel_heros.concat(DC_Heors)
+console.log(MC);
